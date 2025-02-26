39790507115e30a05defec201c3734bb34fbd706
Sri Venkat Chennu <chennusri13@gmail.com>
2025-02-26 14:29:24 +0530

    added frontend url to backend

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const server = http.createServer(app)
 //create socket.io instance
 const io = new Server(server,{
   cors:{
-    origin:"http://localhost:5173",//allow requests from this url
+    origin:"https://meet-frontend-ten.vercel.app/",//allow requests from this url
     methods:["POST","GET"]
   }
 }
